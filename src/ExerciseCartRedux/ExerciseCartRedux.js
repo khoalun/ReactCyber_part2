0c704c45909ad6bffd69b721963d412af69a68e4
Khoa Bui <48984204+khoalun@users.noreply.github.com>
2022-01-15 20:06:31 +0200

    Update ExerciseCartRedux.js

--- src/ExerciseCartRedux/ExerciseCartRedux.js
+++ src/ExerciseCartRedux/ExerciseCartRedux.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import CartRedux from "./CartRedux";
 import ProductListsRedux from "./ProductListsRedux";
 import { connect } from "react-redux";
